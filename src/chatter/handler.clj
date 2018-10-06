c6ce283d41226258df0c433b51c7eac6248b988a
Fabrgu <fabiola.r.gutierrez@gmail.com>
2018-10-06 15:58:00 -0500

    added bootstrap and form

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,18 +2,48 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.page :as page]))
+            [hiccup.page :as page]
+            [hiccup.form :as form]
+            [ring.middleware.params :refer [wrap-params]]))
+
+(def chat-messages (atom '()))
+
 (defn generate-message-view
   "This generates the HTML for displaying messages"
-  []
+  [messages]
   (page/html5
    [:head
-    [:title "chatter"]]
+    [:title "chatter"]
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-js  "//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js")
+    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (page/include-css "/chatter.css")]
    [:body
-    [:h1 "Our Chat App"]]))
-    (defroutes app-routes
-      (GET "/" [] (generate-message-view))
-      (route/not-found "Not Found"))
+    [:h1 "Our Chat App"]
+    [:p (form/form-to
+        [:post "/"]
+        "Name: " (form/text-field "name")
+        "Message: " (form/text-field "msg")
+        (form/submit-button "Submit"))]
+    [:p
+      [:table#messages.table.table-hover
+      (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
+
+(defn update-messages!
+  "This will update a message list atom"
+   [messages name new-message]
+   (swap! messages conj {:name name :message new-message}))
+
+(defroutes app-routes
+  (GET "/" [] (generate-message-view @chat-messages))
+  (POST "/" {params :params}
+          (let [name-param (get params "name")
+                msg-param (get params "msg")
+                 new-messages (update-messages! chat-messages name-param msg-param)]
+                 (generate-message-view new-messages)
+    ))
+  (route/resources "/")
+  (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-params app-routes site-defaults))
