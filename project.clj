dcc3c37f7d0798387f6d7d601d06acd02889947a
Fabrgu <fabiola.r.gutierrez@gmail.com>
2018-10-06 13:56:19 -0500

    added hiccup library

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler chatter.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+  {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [hiccup "1.0.5"]]}})
